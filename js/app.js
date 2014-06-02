f87689092bfd2a367104def92d9de1cc8d56bd70
Wojciech Grabowski <Wojciech.Grabowski@smart4aviation.aero>
2014-06-02 20:32:00 +0200

    Graphs improvements

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ statsApp = {
 		statsApp.attachEvents();
 	},
 	attachEvents : function() {
+		statsApp.loadButton.onclick = function(e){
+			statsApp.fileInput.click();
+		};
 		statsApp.fileInput.onchange = function(e) {
 			var allLoaded = Q.defer();
 			var allCleaned = allLoaded.promise;
@@ -15,19 +18,39 @@ statsApp = {
 				cleanData = null;
 				
 			});
+			statsApp.recalcButton.disabled = false;
 			
-		}
+		};
+		statsApp.canvasBg.onchange = function(e) {
+			statsApp.canvas.style.background = e.target.value;
+			
+		};
 		
 		for(var i=0,j=button = statsApp.showButtons.length; i<j; i++){
 		  var button = statsApp.showButtons[i];
 		  button.onclick = function(e){
 				statsApp.showStats(e.target.getAttribute("data-show"));  	
-		  }
+		  };
+		};
+		for(var i=0,j=button = statsApp.drawButtons.length; i<j; i++){
+		  var button = statsApp.drawButtons[i];
+		  button.onclick = function(e){
+				var set = prepareDataset(statsApp.stats,button.getAttribute("data-draw"));  	
+				drawDataset(set);
+		  };
 		};
 		statsApp.recalcButton.onclick = function(){
-			console.log("cliock");
+			var i = 0,j=0;
 			statsApp.calcStats();
-		}
+			for(i=0,j = statsApp.drawButtons.length; i<j; i++){
+				console.log(statsApp.drawButtons[i]);
+				statsApp.drawButtons[i].disabled = false;
+			}
+			for(i=0,j= statsApp.showButtons.length; i<j; i++){
+				console.log(statsApp.showButtons[i]);
+				statsApp.showButtons[i].disabled = false;
+			}
+		};
 	},
 	calcStats : function() {
 	  	statsApp.stats = mstats.calc.getDailyStats(statsApp.data);
@@ -38,12 +61,16 @@ statsApp = {
 	  	statsApp.chartView.innerHTML = h;	
 
 	},
+	loadButton : document.querySelector("#load"),
 	fileInput : document.querySelector("input#csv"),
 	showButtons : document.querySelectorAll("button[data-show]"),
+	drawButtons : document.querySelectorAll("button[data-draw]"),
 	recalcButton : document.querySelector("button#recalc"),
+	canvas : document.querySelector("canvas"),
+	canvasBg : document.querySelector("#canvasBg"),
 	data : {},
 	chartView:document.querySelector("#charts .content")
-}
+};
 
 statsApp.init();
 
--- js/graphs.js
+++ js/graphs.js
@@ -1,44 +1,97 @@
 var graphs = document.querySelector("#graphs");
+var legend = document.querySelector("#legend");
+
+var canvasSize = {
+	x : graphs.width - 100,
+	y : graphs.height
+};
+var gridCount = {
+	x : 15,
+	y : 90
+};
+var scale = {
+	x : canvasSize.x / gridCount.x,
+	y : canvasSize.y / gridCount.y,
+};
 var ctx = graphs.getContext("2d");
 
-function drawDataset(dataset){
-	for(key in dataset){
-		console.log(key,statusColors[key]);
-		drawChart(dataset[key],statusColors[key],30);
+function drawDataset(dataset) {
+	graphs.width = graphs.width;
+	legend.width = legend.width;
+	var count = 1;
+	var ctx = legend.getContext("2d");
+	for (key in dataset) {
+		console.log(key, statusColors[key]);
+		drawChart(dataset[key], statusColors[key]);
+		ctx.fillStyle = statusColors[key];
+		ctx.fillText(key, 10, 30 * count);
+		count++;
 	}
+	drawGrid();
 }
 
-function drawChart(dataset, color,labelOffset) {
+function drawGrid() {
+	var ctx = graphs.getContext("2d");
+	for (var i = 0; i < gridCount.x; i++) {
+		ctx.beginPath();
+		ctx.moveTo(i * scale.x, 0);
+		ctx.lineTo(i * scale.x, canvasSize.y);
+		ctx.strokeStyle = "gray";
+		ctx.lineWidth= 1;
+		ctx.stroke();
+	};
+	for (var i = 0; i < gridCount.y; i++) {
+		ctx.beginPath();
+		ctx.moveTo(0, i * scale.y);
+		ctx.lineTo(canvasSize.x, i * scale.y);
+		ctx.strokeStyle = "gray";
+		ctx.lineWidth= 1;
+		ctx.stroke();
+	};
+}
+
+function drawChart(dataset, color) {
 	var ctx = graphs.getContext("2d");
 	ctx.beginPath();
 	ctx.moveTo(0, 0);
 	for (var i = 0, j = dataset.length; i < j; i++) {
-		console.log("draw %d %d",i * labelOffset, dataset[i]);
-		value = isNaN(dataset[i]) ? 0 : dataset[i]*scaleY
-		ctx.lineTo(i * labelOffset, value);
+		value = isNaN(dataset[i]) ? 0 : dataset[i] * scale.y
+		ctx.lineTo(i * scale.x, value);
 	}
 	ctx.strokeStyle = color;
+	ctx.lineWidth= 3;
 	ctx.stroke();
+
 }
 
-function prepareDataset(originalData,field){
-	var mapped = [],dataset = {};
-	mapped = originalData.map(function(item){return {label:item.day,data:item.data[field]}});
-	
-	for(var i=0,j=mapped.length; i<j; i++){
-	  data = mapped[i].data;
-	  for(key in data){
-	  	if(typeof dataset[key] === "undefined") dataset[key]= [];
-	  	dataset[key][i]=data[key];
-	  }
+function prepareDataset(originalData, field) {
+	var mapped = [], dataset = {};
+	mapped = originalData.map(function(item) {
+		return {
+			label : item.day,
+			data : item.data[field]
+		}
+	});
+
+	for (var i = 0, j = mapped.length; i < j; i++) {
+		data = mapped[i].data;
+		for (key in data) {
+			if ( typeof dataset[key] === "undefined")
+				dataset[key] = [];
+			dataset[key][i] = data[key];
+		}
 	};
 	return dataset;
 }
 
+
 statusColors = {
-	"new":"salmon",
-	"reopened":"red",
-	"resolved":"steelblue",
-	"closed":"gray"
-	};
-scaleY=30;
+	"new" : "pink",
+	"reopened" : "red",
+	"resolved" : "steelblue",
+	"closed" : "gray",
+	"in progress" : "lime",
+	"code review" : "violet",
+	"client feedback" : "yellow"
+};
+
